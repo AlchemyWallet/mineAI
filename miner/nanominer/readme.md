8237f27ee5cad1c2490390796e525f7776a49db9
DarkStar <darkstar@unisocial.net>
2021-02-28 02:41:26 -0500

    2nd

---
+++ miner/nanominer/readme.md
@@ -0,0 +1,2 @@
+https://github.com/nanopool/nanominer/releases
+https://help.nanopool.org/article/27-nanominer-amd-nvidia
---
+++ miner/t-rex/ssl.t-rex.sh
@@ -0,0 +1,4 @@
+address=0x39ac580865cC67bb2ec4e04385f9dc3c462B511b
+pool=eth-us-east1.nanopool.org
+./t-rex -a ethash -o stratum+ssl://$pool:9433 -u $address
+
